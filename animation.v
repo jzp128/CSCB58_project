8d3648e438a0516c88312cc49e2caea75c3abeb1
Yilun Wu <wuyilun4@IITS-I402-19.utsc-labs.utoronto.ca>
2019-03-18 18:26:01 -0400

    add checklane

--- animation.v
+++ animation.v
@@ -1,3 +1,82 @@
+module PianoTapper(SW,KEY,HEX0, HEX1, HEX2, HEX3, HEX4,HEX5);
+input [3:0] KEY;
+input [9:0] SW;
+
+// draw the game
+drawGame startGame(x1,y1,x2,y2,x3,y3,x4,y4,go,done,clock,plot, RX, RY,colour,ledr, gd,res)
+
+endmodule
+
+module clock(SW, KEY, CLOCK_50, HEX0);
+    input [9:0] SW;
+    input CLOCK_50;
+    
+    output [7:0] HEX0;
+	 
+
+    wire [3:0] counterout;
+
+    counter myCounter(SW[9], CLOCK_50, SW[1:0], SW[8], counterout);
+    hex_display hex0(counterout, HEX0);
+	 
+endmodule
+
+module counter(enable, clk, freq, clear_b, q);
+    input enable, clk, clear_b;
+    input [1:0] freq;
+    output [3:0] q;
+
+    wire [27:0] w1hzout;drawGame
+    wire [27:0] w05hzout;
+
+    wire [27:0] w025hzout;
+
+    ratedivider rd1hz(enable, 28'd49999999, clk, clear_b, w1hzout);
+    ratedivider rd05hz(enable, 28'd99999999, clk, clear_b, w05hzout);
+    ratedivider rd025hz(enable, 28'd199999999, clk, clear_b, w025hzout);
+	 
+	 ratedivider rd4hz(enable, 28'd12499999, clk, clear_b, w1hzout);
+    reg ratedividerout;
+    always @(*)
+        begin 
+            case(freq)
+                2'b00: ratedividerout = enable;
+                2'b01: ratedividerout = (w1hzout == 0) ? 1 : 0;
+                2'b10: ratedividerout = (w05hzout == 0) ? 1 : 0;
+                2'b11: ratedividerout = (w025hzout == 0) ? 1 : 0;
+            endcase
+        end
+    displaycounter myDisplay(ratedividerout, clk, clear_b, q);
+endmodule
+
+module ratedivider(enable, load, clk, clear_b, q);
+    input enable, clk, clear_b;
+    input [27:0] load;
+    output reg [27:0] q;
+
+    always @(posedge clk)
+         begin
+            if (clear_b == 1'b0)
+                q <= load;
+            else if (q == 0)
+                q <= load;
+            else if (enable == 1'b1)
+                q <= q - 1'b1;
+    end
+endmodule
+
+module checklane(input [11:0] screenstate,input input currstate, input [7:0] X, input [7:0] Y, output isgood);
+	assign good = screenstate[15] || 
+	if (currstate == finish)
+		X <= 1'b0;
+		Y <= 1'b0;
+	else if (currstate == draw)
+		
+		
+		
+endmodule
+
+
 module drawGame(x1,y1,x2,y2,x3,y3,x4,y4,go,done,clock,plot, RX, RY,colour,ledr, hex4, hex5, gd,res);
 	
 	// Declaration of all necessary inputs/outputs including x and y of tiles , 
@@ -12,7 +91,7 @@ module drawGame(x1,y1,x2,y2,x3,y3,x4,y4,go,done,clock,plot, RX, RY,colour,ledr, 
 	output reg done;
 	output reg [7:0]RX; // general input of Xs
 	output reg [6:0]RY;
-	output reg [1:0]colour;
+	output reg [1:0]colo15 seconds for a touch stimulus.ur;
 	output [3:0]ledr;
 	output [6:0]hex4;
 	output [6:0]hex5;
@@ -25,7 +104,7 @@ module drawGame(x1,y1,x2,y2,x3,y3,x4,y4,go,done,clock,plot, RX, RY,colour,ledr, 
 	reg [2:0]present;
 	reg [2:0] next;
 	
-	//TODO: Counter 
+	//TODO: Counter ratedivider rd1hz(enable, 28'd49999999, clk, clear_b, w1hzout);
 	
 	// used to keep track of which pixel is being drawn of a specfic tile 
 	reg [5:0]counterx;
@@ -49,21 +128,25 @@ module drawGame(x1,y1,x2,y2,x3,y3,x4,y4,go,done,clock,plot, RX, RY,colour,ledr, 
 	
 	always @(*)
 	begin gameStates:
-		case (present)
+		case (present)if (fd)
+				next = eraseS;
+			else 
 			
 			resetS:
 			if(go)
 				next = check;
 			else if(gd)
 				next = doneS;
-			else
-				next = resetS;
+			else // general input of Xs
+				next = resetS;if (fd)
+				next = eraseS;
+			else 
 			
 			checkS:
-			if (fd)
+			if (gd)
+				next = finishS;
+			else
 				next = eraseS;
-			else if (gd)
-				next = doneS;
 			
 			eraseS:
 			if (ed)
@@ -88,15 +171,18 @@ module drawGame(x1,y1,x2,y2,x3,y3,x4,y4,go,done,clock,plot, RX, RY,colour,ledr, 
 			else if (gd && !res)
 				next = checkS;
 			
-			doneS:
-			if (gd)
-				next = doneS;
-			else if (res && gd)
-				next = resetS;
+//			doneS:
+//			if (gd)
+//				next = doneS;
+//			else if (res && gd)
+//				next = resetS;
 			
 			default: next = reset
 			
 		endcase
+		
+		// if (check)
+			//check other stuff
 		
 		
 		
