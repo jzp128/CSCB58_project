8461a2bfd62cfd8418fd3264a8a324956dcf6289
jzp128 <jimmypang8@gmail.com>
2019-04-03 20:42:27 -0400

    reworked states

--- hi/output_files/tiles.v
+++ hi/output_files/tiles.v
@@ -5,10 +5,10 @@ module tiles(
                 KEY,
                 SW,
                 LEDG,
-					 LEDR,
+                     LEDR,
                 HEX0,
                 HEX1,
-					 HEX4,
+                     HEX4,
                 // The ports below are for the VGA output.  Do not change.
                 VGA_CLK,                                                //      VGA Clock
                 VGA_HS,                                                 //      VGA H_SYNC
@@ -19,14 +19,14 @@ module tiles(
                 VGA_G,                                                  //      VGA Green[9:0]
                 VGA_B                                                   //      VGA Blue[9:0]
         );
-
+ 
         input   CLOCK_50;                               //      50 MHz
         input   [17:0]   SW;
         input   [3:0]   KEY;
-		  output   [6:0]   HEX0, HEX1, HEX4;
-		  output  [17:0] 	LEDR;
+          output   [6:0]   HEX0, HEX1, HEX4;
+          output  [17:0]    LEDR;
         output  [3:0]   LEDG;
-
+ 
         // Declare your inputs and outputs here
         // Do not change the following outputs
         output                  VGA_CLK;                                //      VGA Clock
@@ -37,18 +37,18 @@ module tiles(
         output  [9:0]   VGA_R;                                  //      VGA Red[9:0]
         output  [9:0]   VGA_G;                                  //      VGA Green[9:0]
         output  [9:0]   VGA_B;                                  //      VGA Blue[9:0]
-
+ 
         wire resetn;
         assign resetn = SW[16];
-
+ 
         // Create the colour, x, y and writeEn wires that are inputs to the controller.
         wire [2:0] colour;
         wire [7:0] x;
         wire [6:0] y;
         wire [7:0] score;
         wire [1:0] lives;
-		  wire [3:0] level;
-
+          wire [3:0] level;
+ 
         // Create an Instance of a VGA controller - there can be only one!
         // Define the number of colours as well as the initial background
         // image file (.MIF) for the controller.
@@ -72,7 +72,7 @@ module tiles(
                 defparam VGA.MONOCHROME = "FALSE";
                 defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
                 defparam VGA.BACKGROUND_IMAGE = "black.mif";
-                
+               
         control(
             .clk(CLOCK_50),
             .resetn(resetn),
@@ -89,47 +89,47 @@ module tiles(
             .col(colour),
             .score(score),
             .lives(lives),
-				.out_lives(LEDG[2:0]),
-				.level(level)
+            .out_lives(LEDG[2:0]),
+            .level(level)
         );
-        
+       
         // display score on HEX0, HEX1
         hex_decoder h1(
             .hex_digit(score[7:4]),
             .segments(HEX1[6:0])
         );
-        
+       
         hex_decoder h0(
             .hex_digit(score[3:0]),
             .segments(HEX0[6:0])
         );
-		  
+         
         hex_decoder h4(
             .hex_digit(level[3:0]),
             .segments(HEX4[6:0])
         );
-
+ 
 endmodule
-
-
+ 
+ 
 module control(
     input clk,
     input resetn,
     input pause,
     input mode,
     input key0, key1, key2, key3,
-	 output [16:0] beat_map_display,
-	 output reg on_off,
-
-    output reg [7:0] x, 
-    output reg [6:0] y, 
-    output reg [2:0] col, 
+     output [16:0] beat_map_display,
+     output reg on_off,
+ 
+    output reg [7:0] x,
+    output reg [6:0] y,
+    output reg [2:0] col,
     output reg [7:0] score,
     output reg [1:0] lives,
-	 output [2:0] out_lives,
-	 output reg [3:0] level);
-    
-	 
+     output [2:0] out_lives,
+     output reg [3:0] level);
+   
+     
     reg [4:0] state;
     reg [4:0] prev_state;
     reg [18:0] draw_screen;
@@ -138,6 +138,7 @@ module control(
     reg [6:0] line_counter;
     reg [7:0] x_d, x_p, p_x; // x position of tiles
     reg [6:0] y_d, y_p, p_y; // y postition of tiles
+ 
     reg [2:0] t_c; // colour of tiles
     reg [1:0] c_tile; // which tile is being drawn
     reg start, check_score, check; // boolean for when player started
@@ -145,45 +146,45 @@ module control(
     wire [1:0] tile; // select which tile to draw
     reg [7:0] max_score; // max score for a level
     wire p0, p1, p2, p3; // for pressing correct key
-    
+   
     wire [20:0] song;
-
+ 
     wire en, counter;
     assign tile = num[4:3]; // random tile number
-	 
-	 reg [8:0] beat_map = 9'b0100001010;
-	 reg [7:0] read_counter = 8'd0;
-	 assign beat_map_display[8:0] = beat_map[8:0];
-	 assign beat_map_display[16:9] = read_counter[7:0];
-	 
-	 wire [7:0] read_counter_next;
-	 
-	 
+     
+     reg [800:0] beat_map = 801'b0100000000100000000;
+     reg [7:0] read_counter = 8'd0;
+     assign beat_map_display[8:0] = beat_map[8:0];
+     assign beat_map_display[16:9] = state;
+     
+     wire [7:0] read_counter_next;
+     
+     
     display_lives d(
-				.clk(clk),
+                .clk(clk),
             .mode(mode),
             .lives(lives),
             .out(out_lives)
         );
-		  
+         
     generator g(
         .clk(clk),
         .resetn(resetn),
         .data(num)
         );
-    
+   
     counter c(
         .clk(clk),
         .speed(speed),
         .frame(counter)
         );
-		wire one_hz;
-	 counter c_one_hz(
+        wire one_hz;
+     counter c_one_hz(
         .clk(clk),
-        .speed(28'd49999999),
+        .speed(28'd24999999),
         .frame(one_hz)
         );
-
+ 
     check_tile ct(
         .clk(clk),
         .check(check),
@@ -197,8 +198,8 @@ module control(
         .p2(p2),
         .p3(p3)
         );
-
-    
+ 
+   
     localparam START = 5'd0,
                DRAW_LINE = 5'd1,
                RESET = 5'd2,
@@ -217,16 +218,20 @@ module control(
                GAME_OVER = 5'b10000,
                WIN = 5'b10001,
                CLEAR_SCREEN = 5'b10010;
-    
-	 always @(posedge one_hz)
-		on_off <= ~on_off;
-	 
+   
+     always @(posedge one_hz)
+     begin
+        on_off <= ~on_off;
+        if (on_off)
+            beat_map <= beat_map >> 1;
+     end
+     
     always @(posedge clk)
     begin
         x <= 8'b0;
         y <= 7'b0;
         col <= 3'b0;
-		  if (!start) state = START;
+          if (!start) state = START;
         if (resetn) state = RESET;
         if (pause) state = PAUSE;
     case (state)
@@ -235,162 +240,113 @@ module control(
             score <= 8'b0;
             speed <= 28'd999999;
             lives <= 2'b11; // starts with 3 lives
-            level <= 4'b0001;
+				level <= 4'b0001;
 				y <= draw_screen[7:0];
             x <= draw_screen[14:8];
+				p_y = 7'd120;
+				y_d = 7'd120;
             draw_screen <= draw_screen + 1'b1;
             col <= 3'b111; //draw white
             state = DRAW_LINE;
         end
         DRAW_LINE: begin
             y <= 8'd110;
-            if (line_counter < 7'd120) begin
+            if (line_counter < 7'd160) begin
                 x <= line_counter[6:0];
-					 line_counter <= line_counter + 1'b1;
-					 col <= 3'b010;
+                     line_counter <= line_counter + 1'b1;
+                     col <= 3'b010;
             end
             else begin
                 line_counter <= 7'b0;
-                state = DRAW_TILES;
+                state = DRAW_T;
             end
         end
         // clear everything
-         RESET: begin
-            if (draw_screen < 16'd65535) begin
-                y <= draw_screen[7:0];
-                x <= draw_screen[14:8];
-                draw_screen <= draw_screen + 1'b1;
-					 col <= 3'b111;
-            end
-            else begin
-                draw_screen <= 16'b0;
-					 tile_counter <= 9'b0;
-					 start <= 1'b0;
-                /*
-					 score <= 8'b0;
-                speed <= 28'd199999999;
-                lives <= 2'b11; // starts with 3 lives
-                level <= 4'b0001;
-                state = DRAW_TILES;
-					 */
-            end
-        end
+			RESET: begin
+				if (draw_screen < 16'd65535) begin
+					 y <= draw_screen[7:0];
+					 x <= draw_screen[14:8];
+					 draw_screen <= draw_screen + 1'b1;
+							col <= 3'b111;
+				end
+				else begin
+					 draw_screen <= 16'b0;
+							tile_counter <= 9'b0;
+							start <= 1'b0;
+					 /*
+							score <= 8'b0;
+					 speed <= 28'd199999999;
+					 lives <= 2'b11; // starts with 3 lives
+					 level <= 4'b0001;
+					 state = DRAW_TILES;
+							*/
+				end
+		  end
 		  CLEAR_SCREEN: begin
             if (draw_screen < 16'd65535) begin
                 y <= draw_screen[7:0];
                 x <= draw_screen[14:8];
                 draw_screen <= draw_screen + 1'b1;
-					 col <= 3'b111;
-					 
+                col <= 3'b111;
+                     
             end
             else begin
                 draw_screen <= 16'b0;
-					 tile_counter <= 9'b0;
-					 state = DRAW_LINE;
-				end
-		  end
-		  
+                     tile_counter <= 9'b0;
+                     state = DRAW_LINE;
+                end
+			end
+         
         PAUSE: begin
              if (!pause)
                  state = prev_state;
         end
-        DRAW_TILES: begin
-        // choose a random tile to draw
-            if (tile == 2'b00)
-                state = INIT_T1;
-//            else if (tile == 2'b01)
-//                state = INIT_T2;
-//            else if (tile == 2'b10)
-//                state = INIT_T3;
-//            else
-//                state = INIT_T4;
-        end
         INIT_T1: begin
         // set where to draw first tile
             p_x <= 8'b0;
-            p_y <= 7'd1;
+            p_y <= 7'd0;
             x_d <= 8'b0;
-            y_d <= 7'd1;
+            y_d <= 7'd0;
             t_c <= 3'b000; // tiles are black
             //c_tile <= 2'b0;
             state = DRAW_T;
         end
-//        INIT_T2: begin
-//        // set where to draw second tile
-//            p_x <= 8'b0;
-//            p_y <= 7'd38;
-//            x_d <= 8'b0;
-//            y_d <= 7'd38;
-//            t_c <= 3'b010;
-//            c_tile <= 2'b01;
-//            state = DRAW_T;
-//        end
-//        INIT_T3: begin
-//        // set where to draw third tile
-//            p_x <= 8'b0;
-//            p_y <= 7'd65;
-//            x_d <= 8'b0;
-//            y_d <= 7'd65;
-//            t_c <= 3'b001;
-//            c_tile <= 2'b10;
-//            state = DRAW_T;
-//        end
-//        INIT_T4: begin
-//        // set where to draw fourth tile
-//            p_x <= 8'b0;
-//            p_y <= 7'd92;
-//            x_d <= 8'b0;
-//            y_d <= 7'd92;
-//            t_c <= 3'b110;
-//            c_tile <= 2'b11;
-//            state = DRAW_T;
-//        end
-	
         DRAW_T: begin
             prev_state <= state;
-				if (p_y < 8'd120) begin
-					if (tile_counter < 9'd256) begin
-						 y <= p_y + tile_counter[7:4];
-						 x <= p_x + tile_counter[3:0];
-						 tile_counter <= tile_counter + 1'b1;
-						 
-						 col <= 3'b000; // DRAWS BLACK
-					end
-					else begin
-						 tile_counter <= 9'b0;
-						 p_y <= p_y + 1'b1;
-						 state = FRAME;
-					end
-
-					if (p_y > 105 && p_y < 119) begin
-						check <= 1'b1;
-						if (p0 || p1 || p2 || p3) begin
-							state = CHECK_TILE;
-						end
-					end
-					else begin
-						check <= 1'b0;
-					end
-				
-				end
-				else begin
-					state = CHECK_TILE;
-				end
-				
-
-
+                // if tile not at bottom
+ 
+                if (p_y < 8'd120) begin
+ 
+                    // if not done drawing
+                    if (tile_counter < 9'd256) begin
+                         y <= p_y + tile_counter[7:4];
+                         x <= p_x + tile_counter[3:0];
+                         tile_counter <= tile_counter + 1'b1;
+                         col <= 3'b000; // DRAWS BLACK
+                    end
+ 
+                    else begin
+                         tile_counter <= 9'b0;
+                         p_y <= p_y + 1'b1;
+                         state = FRAME;
+                    end
+ 
+                end
+                else
+                    state = FRAME;
+               
+ 
+ 
         end
         FRAME: begin
-				if (one_hz)
-				begin
-					beat_map <= beat_map >> 1;
-				end
             if (counter)
                 state = ERASE_TILE;
-			end
+            end
         ERASE_TILE: begin
             prev_state <= state;
-            if (y_d < 8'd144) begin
+                // if delete area is not at the end
+            if (y_d < 8'd120) begin
+                     // if not done erasing
                 if (tile_counter < 9'd256) begin
                     y <= y_d + tile_counter[7:4];
                     x <= x_d + tile_counter[3:0];
@@ -400,46 +356,33 @@ module control(
                 else begin
                     tile_counter <= 9'b0;
                     y_d <= y_d + 1'b1;
-                    state = DRAW_T;
+                          state = CHECK_TILE;
                 end
             end
-            else
-                state = CHECK_TILE;
-					 
-			end
+                else
+                    state = CHECK_TILE;
+                     
+            end
         CHECK_TILE: begin
-				
-				
             prev_state <= state;
-				state = CHECK_LEVEL;
-            // if (p0 || p1 || p2 || p3) begin
+            if (p_y > 105 && p_y < 119) begin
+                    check = 1'b1;
+            end
+			   else
+                    check = 1'b0;
+               
             if (p0) begin
-                score <= score + 1'b1;
-                check_score <= 1'b1;
+					  score <= score + 1'b1;
+					  // moves the tile to the bottom
+					  p_y <= 7'd120;
+					  y_d <= 7'd120;
+				 end
+				 if (beat_map[0] == 1'b0)
+					  state = DRAW_LINE;
+				 else
+					  state = INIT_T1;
+					
             end
-            // at infinite mode, decrease score if tile was not pressed correctly
-            else if (mode == 1'b1 && check_score) begin
-                if (score > 0)
-						score <= score - 1'b1;
-					 if (score == 0)
-						state = GAME_OVER;
-            end
-            // normal mode, decrease life
-            else if (mode == 1'b0) begin
-                lives <= lives - 1'b1;
-					 if (lives == 0)
-						state = GAME_OVER;
-            end
-            
-			end
-        CHECK_LEVEL: begin
-            // 15 levels max
-            if (level == 4'b1111) begin
-                state = WIN;
-            end
-            else
-                state = CLEAR_SCREEN;
-        end
         WIN: begin
             prev_state <= state;
             if (draw_screen < 16'd65535) begin
@@ -469,13 +412,13 @@ module control(
     endcase
     end
 endmodule
-
+ 
 //check if player hit the right key
 module check_tile(clk, check, tile, key0, key1, key2, key3, p0, p1, p2, p3);
     input clk, check, key0, key1, key2, key3;
     input [1:0] tile;
     output reg p0, p1, p2, p3;
-    
+   
     always @(posedge clk)
     begin
         if (check && tile == 2'b00 && key0) begin
@@ -507,19 +450,20 @@ module check_tile(clk, check, tile, key0, key1, key2, key3, p0, p1, p2, p3);
             p1 <= 1'b0;
             p2 <= 1'b0;
             p3 <= 1'b0;
+				//lives <= lives - 1'd1;
         end
     end
 endmodule
-
+ 
 // fibonacci random number generator from https://stackoverflow.com/questions/14497877/how-to-implement-a-pseudo-hardware-random-number-generator
 module generator(
     input clk,
     input resetn,
-    
+   
     output reg [4:0] data);
-    
+   
     reg [4:0] data_next;
-    
+   
     always @(*)
     begin
         data_next[4] = data[4]^data[1];
@@ -528,23 +472,23 @@ module generator(
         data_next[1] = data[1]^data_next[3];
         data_next[0] = data[0]^data_next[2];
     end
-    
+   
     always @(posedge clk)
-	 begin
+     begin
         if (resetn)
             data <= 5'h1f;
         else
             data <= data_next;
-	end
+    end
 endmodule
-
+ 
 module counter(
     input clk,
     input [27:0] speed,
     output frame);
-
+ 
     reg [27:0] counter;
-
+ 
     always @(posedge clk)
     begin
         if (counter == 0)
@@ -552,16 +496,16 @@ module counter(
         else
             counter <= counter - 1'b1;
     end
-
+ 
     assign frame = (counter == 0) ? 1 : 0;
-
+ 
 endmodule
-
+ 
 module display_lives(clk, mode, lives, out);
     input clk, mode;
     input [1:0] lives;
     output reg [2:0] out;
-    
+   
     always @(posedge clk)
     begin
         if (~mode) begin
@@ -578,11 +522,11 @@ module display_lives(clk, mode, lives, out);
             out <= 3'b0;
     end
 endmodule
-
+ 
 module hex_decoder(hex_digit, segments);
     input [3:0] hex_digit;
     output reg [6:0] segments;
-
+ 
     always @(*)
         case (hex_digit)
             4'h0: segments = 7'b100_0000;
